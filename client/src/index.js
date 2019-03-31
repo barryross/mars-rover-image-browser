026a349102101d757153246cc5bdcfaadbcc2cd0
Barry Ross <barry@4d-media.ca>
2019-03-31 11:25:54 -0700

    adds some boilerplate for index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, compose, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import reducer from './reducers';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {}
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  reducer,
+  initialState,
+  compose(
+    composeWithDevTools(
+    applyMiddleware(
+      sagaMiddleware,
+    ),
+  ))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
