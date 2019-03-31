ce27fbc418340e9601b9235b81045df128e3f7c7
Barry Ross <barry@4d-media.ca>
2019-03-31 13:38:51 -0700

    simplifies Curiosity saga

--- client/src/sagas/rovers.js
+++ client/src/sagas/rovers.js
@@ -2,8 +2,8 @@ import httpClient from '../utilities/httpClient'
 import { put, takeEvery } from 'redux-saga/effects'
 
  function* workerGetCurosityImages(){
-	const goals = yield httpClient.get('/api/rovers/curiosity/photos?date=2017-01-01')
-  yield put({ type: "GET_CURIOSITY_SUCCESS", data:goals.data });
+	const data = yield httpClient.get('/api/rovers/curiosity?date=2017-01-01')
+  yield put({ type: "GET_CURIOSITY_SUCCESS", data});
 }
 
 export function* watchRovers(){
