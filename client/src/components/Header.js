1ea735217711fb2dc686082409f5afa83427c1d8
Barry Ross <barry@4d-media.ca>
2019-03-31 11:32:04 -0700

    adds Header component (semantic-ui boiler)

---
+++ client/src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+export default class Header extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu>
+        <Menu.Item
+          name='rovers'
+          active={activeItem === 'rovers'}
+          onClick={this.handleItemClick}
+        >
+          Rovers
+        </Menu.Item>
+
+        <Menu.Item name='reviews' active={activeItem === 'about'} onClick={this.handleItemClick}>
+          About
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
