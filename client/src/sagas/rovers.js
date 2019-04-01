700cde270fe53addb5cc961d2da246f133963df1
Barry Ross <barry@4d-media.ca>
2019-03-31 21:20:33 -0700

    rovers saga now indicates global loading status

--- client/src/sagas/rovers.js
+++ client/src/sagas/rovers.js
@@ -3,8 +3,10 @@ import { put, takeEvery } from 'redux-saga/effects'
 
  function* workerGetRoverImages(data){
 	const { rover_name, date } = data;
+	yield put({ type: "LOADING", data:true}); 
 	const response = yield httpClient.get(`/api/rovers/${rover_name}?date=${date}`)
-  yield put({ type: `GET_${rover_name}_SUCCESS`.toUpperCase(), response});
+	yield put({ type: `GET_${rover_name}_SUCCESS`.toUpperCase(), response});
+	yield put({ type: "LOADING", data:false});
 }
 
 export function* watchRovers(){
