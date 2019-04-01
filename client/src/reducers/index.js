bf52ff6d7f6c5cd32364d49ee45987ac7ba0b2c2
Barry Ross <barry@4d-media.ca>
2019-03-31 23:18:48 -0700

    moves router reducer into root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,15 +1,18 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router'
 
 import curiosity from './curiosity'
 import opportunity from './opportunity'
 import spirit from './spirit'
 import ui from './ui'
 
-const rootReducer = combineReducers({
+const rootReducer = (history) => combineReducers({
 	Curiosity:curiosity,
 	Opportunity:opportunity,
 	Spirit:spirit,
 	ui,
+	router: connectRouter(history)
+
 });
 
 export default rootReducer;
