df660669d117515a57bee31d6cce39b33039b7ac
Barry Ross <barry@4d-media.ca>
2019-03-31 21:21:32 -0700

    adds action creator to handle all rovers

---
+++ client/src/actions/rovers.js
@@ -0,0 +1,15 @@
+export const getRoverImages = (rover_name, date) =>{
+	return {
+		type: "GET_ROVER_IMAGES",
+		rover_name,
+		date
+ }
+}
+
+export const setSelectedDate = (rover_name, date) =>{
+
+	return {
+		type: `SET_DATE_${rover_name}`.toUpperCase(),
+		date
+ }
+}
