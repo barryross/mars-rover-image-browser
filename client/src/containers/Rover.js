f5300bc5d7dae04304383d6829b2712fd1955d00
Barry Ross <barry@4d-media.ca>
2019-03-31 21:19:59 -0700

    removes unnecessary containers

---
+++ client/src/containers/Rover.js
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+import {withRouter} from 'react-router'
+import Rover from '../components/Rover'
+import { getRoverImages, setSelectedDate} from '../actions/rovers'
+
+const mapStateToProps = (state, ownProps) => {
+    return {	
+				images:state[ownProps.name].images,
+				currentlySelectedDate:state[ownProps.name].date,
+				loading:state.ui.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+			getRoverImages: (rover_name, date) => {
+				dispatch(getRoverImages(rover_name, date))
+			},
+			setSelectedDate: (rover_name, date) => {
+				dispatch(setSelectedDate(rover_name, date))
+			}
+		}
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Rover));
