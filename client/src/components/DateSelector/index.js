dcf93d7fd7d6ac6f17a3bdf8f38b9a7e30190a43
Barry Ross <barry@4d-media.ca>
2019-03-31 21:21:49 -0700

    adds DateSelector wrapper component for datepicker

---
+++ client/src/components/DateSelector/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import DatePicker from "react-datepicker";
+import { Button, Icon } from 'semantic-ui-react'
+import "react-datepicker/dist/react-datepicker.css";
+
+const DateSelector = ({selectedDate, handleChange, className}) => {
+	return (
+		<div className={className}>
+		<Icon name="calendar"/>
+			<DatePicker
+			customInput={<Button>{selectedDate}</Button>}
+			showYearDropdown
+			className="DatePicker"
+			selected={new Date(selectedDate)}
+			onChange={handleChange}
+		/>
+	</div>
+	)
+}
+
+DateSelector.propTypes = {
+  className: PropTypes.string
+};
+		 
+export default DateSelector
