93652891ab7b313d9dbcfe64f903258e7668c109
Barry Ross <barry@4d-media.ca>
2019-04-23 13:24:29 -0700

    refactors uiReducer

--- client/src/reducers/ui.js
+++ client/src/reducers/ui.js
@@ -1,12 +1,13 @@
 
 import { fromJS } from 'immutable';
+import { LOADING } from '../actionTypes'
 
 const initialState = fromJS({
 	loading:false
 })
 export default (state = initialState, action) => {
   switch (action.type) {
-		case "LOADING":
+		case LOADING:
 		return state.set('loading', action.data)
    default:
     return state
