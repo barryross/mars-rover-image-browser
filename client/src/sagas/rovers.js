d317028b41f3951f1c7cdf0b88534578635e9c5e
Barry Ross <barry@4d-media.ca>
2019-04-23 13:24:00 -0700

    abstracts out action creator calls from saga

--- client/src/sagas/rovers.js
+++ client/src/sagas/rovers.js
@@ -1,13 +1,14 @@
 import httpClient from '../utilities/httpClient'
 import {GET_ROVER_IMAGES} from '../actionTypes/'
+import { setLoading } from '../actions'
 import { put, takeEvery } from 'redux-saga/effects'
 
  function* workerGetRoverImages(data){
 	const { rover_name, date } = data;
-	yield put({ type: "LOADING", data:true}); 
+	yield put(setLoading(true)); 
 	const response = yield httpClient.get(`/api/rovers/${rover_name}?date=${date}`)
 	yield put({ type: `GET_${rover_name}_IMAGES_SUCCESS`.toUpperCase(), response}); // Works for all three different rovers
-	yield put({ type: "LOADING", data:false});
+	yield put(setLoading(false));
 }
 
 export function* watchRovers(){
